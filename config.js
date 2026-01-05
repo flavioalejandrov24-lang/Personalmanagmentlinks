15ded4e64254302ff6e22f471d61f3495950f4be
flavioalejandrov24-lang <flavioalejandrov24@gmail.com>
2026-01-04 23:29:38 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    url: 'https://nhqwwqfhazpzlksiftmd.supabase.co',  
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ocXd3cWZoYXpwemxrc2lmdG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1ODExMDYsImV4cCI6MjA4MzE1NzEwNn0.5GbOjyTy9Xa9_oB71pdNfiEdRtjxbixQ6khFzl4jn8o'
+};
